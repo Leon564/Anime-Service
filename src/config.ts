acdd43a156f995a2fbbdb058959654f6d4011cab
Leon564 <nleon564.nl@gmail.com>
2022-11-12 23:02:25 -0600

    fix(): headless

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export default {
     puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
     const browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       ignoreDefaultArgs: ["--disable-extensions"],
       args: [
         "--no-sandbox",
