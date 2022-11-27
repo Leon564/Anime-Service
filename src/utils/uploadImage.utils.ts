12b4c96bcb83a200a280ff7aaf1bd95b98a25a21
Leon564 <nleon564.nl@gmail.com>
2022-11-27 00:35:06 -0600

    fix(): catch error and default image on upload image

--- src/utils/uploadImage.utils.ts
+++ src/utils/uploadImage.utils.ts
@@ -1,15 +1,21 @@
 const _imgbb = require("imgbb");
 const imgbb = _imgbb;
 import "dotenv/config";
-
+import logger from "./logger.utils";
+const defaultImage = "https://i.ibb.co/1Tp6cTn/x1080.jpg";
 const uploadImage = async (image: string) => {
-  const res = await new imgbb(
-    "https://www3.animeflv.net/uploads/animes/covers/3691.jpg"
-  )
-    .setApiKey(process.env.IMGBB_API_KEY)
-    .upload();
+  logger.info("Uploading image to imgbb...");
+  logger.info("Image URL: " + image);
+  try {
+    const res = await new imgbb(image)
+      .setApiKey(process.env.IMGBB_API_KEY)
+      .upload();
 
-  return res.url;
+    return res.url;
+  } catch (e) {
+    logger.error(e);
+    return defaultImage;
+  }
 };
 
 export default uploadImage;
