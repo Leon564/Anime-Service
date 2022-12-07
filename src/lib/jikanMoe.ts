698c92734a69a61b0096088c446f90cc156f7997
Leon564 <nleon564.nl@gmail.com>
2022-12-07 15:30:18 -0600

    refactor(): jikan search

--- src/lib/jikanMoe.ts
+++ src/lib/jikanMoe.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import detectLanguaje from "../utils/detectLanguaje";
 class jikanMoe {
-  getAnimeInfo = async (name: string, limit: number = 5) => {
+  getAnimeInfo = async (name: string) => {
     const q = detectLanguaje(name) === "es" ? name.split(" ")[0] : name;
     const info = await axios.get(
-      `https://api.jikan.moe/v4/anime?q=${q}&limit=${limit}`
+      `https://api.jikan.moe/v4/anime?q=${q}`
     );
     if (info.data.data.length === 0) return null;
     const anime = info.data.data.find((item: any) => {
