8b2c2c130a2bc006156558a313543cd5fbe58896
Leon564 <nleon564.nl@gmail.com>
2022-12-07 15:18:08 -0600

    refactor(): jikan search

--- src/lib/jikanMoe.ts
+++ src/lib/jikanMoe.ts
@@ -1,16 +1,22 @@
 import axios from "axios";
 import detectLanguaje from "../utils/detectLanguaje";
 class jikanMoe {
-  getAnimeInfo = async (name: string) => {
+  getAnimeInfo = async (name: string, limit: number = 5) => {
     const q = detectLanguaje(name) === "es" ? name.split(" ")[0] : name;
     const info = await axios.get(
-      `https://api.jikan.moe/v4/anime?q=${q}&limit=5`
+      `https://api.jikan.moe/v4/anime?q=${q}&limit=${limit}`
     );
     if (info.data.data.length === 0) return null;
-    const anime = info.data.data.find(
-      (item: any) => item.title.toLowerCase() === name.toLowerCase()
-    );
-    return anime || info.data.data[0];
+    const anime = info.data.data.find((item: any) => {
+      for (let i = 0; i < item.titles.length; i++) {
+        const element = item.titles[i];
+        if (element.title.toLowerCase() === name.toLowerCase()) return true;
+      }
+      return false;
+    });
+    console.log(anime);
+
+    return anime || null;
   };
 }
 export default new jikanMoe();
